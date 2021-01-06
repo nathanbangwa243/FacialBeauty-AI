b348216212a338b113edfd839a0d2ad06936713e
Nathan Bangwa <nathanbangwa7@gmail.com>
2021-01-06 05:22:43 +0200

    init Azure Function (api)

---
+++ api/FacialBeauty/__init__.py
@@ -0,0 +1,30 @@
+import logging
+
+import azure.functions as func
+
+import os
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        cwd = os.getcwd()
+
+        listdir = os.listdir(cwd)
+
+        return func.HttpResponse(f"cwd : {cwd}\nlistdir : {listdir}")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
